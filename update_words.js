b2a5302560fbd9d55d41e0cdbf59467f8e037ff5
Gleb <43728100+lounres@users.noreply.github.com>
2020-05-10 16:01:50 +0300

    Renamed DB name.

--- update_words.js
+++ update_words.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('DB.sqlite');
+const db = new sqlite3.Database('DB.db');
 
 const request = require('request');
 request('http://the-hat.appspot.com/api/v2/dictionary/ru', function (error, response, body) {
