187fc2408528abb2c1dc5f096971431a97bb9004
Ivan Gaidai-Turlov <gtivansan@gmail.com>
2020-06-30 13:04:15 +0300

    staging mark

---
+++ src/components/draftMark.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="water-mark">
+	<span>DRAFT</span>
+</div>
+</template>
+
+<style>
+.water-mark {
+    position: fixed;
+    bottom: -10px;
+    text-align: center;
+    width: 100%;
+    color: black;
+    font-size: 50px;
+    opacity: .2;
+    pointer-events: none;
+    z-index: 100;
+    user-select: none;
+}
+</style>
--- src/view.vue
+++ src/view.vue
@@ -2,5 +2,20 @@
 <div>
 	<router-view/>
 	<notifications/>
+	<draft-mark v-if="showMark" />
 </div>
 </template>
+
+<script>
+import draftMark from "_/draftMark.vue"
+import {ENV, STAGING} from "__/config.js"
+
+export default {
+	data: function() {
+		return {
+			showMark: ENV == STAGING
+		}
+	},
+	components: {draftMark}
+}
+</script>
