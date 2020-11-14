03b48e079aa9622bb8df9ebbc7b3a25608661233
Ivan Gaidai-Turlov <gtivansan@gmail.com>
2020-11-14 13:00:29 +0300

    Made sending feedback

--- src/api.js
+++ src/api.js
@@ -1,3 +1,6 @@
+import app from "src/app.js"
+import { VERSION, HASH } from "./version.js"
+
 export async function getFreeKey() {
 	return (await (await fetch("api/getFreeKey")).json()).key
 }
@@ -17,5 +20,32 @@ class RoomInfo {
 
 export async function getRoomInfo(key) {
 	if (key == "") return new RoomInfo();
-	return new RoomInfo(await (await fetch("api/getRoomInfo?key=" + key)).json())
+	return new RoomInfo(await (await fetch("api/getRoomInfo?key=" + key)).json());
+}
+
+export async function sendFeedback(message, collectBrowserData) {
+	let feedback = {};
+
+	if (collectBrowserData) {
+		feedback.appName = navigator.appName;
+		feedback.appVersion = navigator.appVersion;
+		feedback.cookieEnabled = navigator.cookieEnabled;
+		feedback.platform = navigator.platform;
+		feedback.product = navigator.product;
+		feedback.userAgent = navigator.userAgent;
+	}
+
+	feedback.SID = app.socket.id;
+	feedback.version = VERSION;
+	feedback.hash = HASH;
+	feedback.message = message;
+	feedback.appLog = app.appLog;
+
+	return await fetch("feedback", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json; charset=utf-8"
+		},
+		body: JSON.stringify(feedback)
+	});
 }
