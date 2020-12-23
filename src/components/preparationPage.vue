f073373941b3e4c2fdf7f545865d3403b80e0855
feodor <feodor.kuyanov@gmail.com>
2020-12-23 17:09:34 +0300

    Added settings

--- src/components/preparationPage.vue
+++ src/components/preparationPage.vue
@@ -24,25 +24,27 @@
         </button>
       </header>
       <main>
-        <div class="room-actions">
-          <button
-              class="btn btn-transparent"
-              @click="copyKey()">
-            <span class="fas fa-clipboard"></span>
-            Ключ
-          </button>
-          <button
-              class="btn btn-transparent"
-              @click="copyLink()">
-            <span class="fas fa-link"></span>
-            Ссылка
-          </button>
-          <button
-              class="btn btn-transparent leave-room"
-              @click="leaveRoom()">
-            <span class="fas fa-sign-out-alt"></span>
-            Выйти
-          </button>
+        <div class="scrollable">
+          <div class="room-actions">
+            <button
+                class="btn btn-transparent"
+                @click="copyKey()">
+              <span class="fas fa-clipboard"></span>
+              Ключ
+            </button>
+            <button
+                class="btn btn-transparent"
+                @click="copyLink()">
+              <span class="fas fa-link"></span>
+              Ссылка
+            </button>
+            <button
+                class="btn btn-transparent leave-room"
+                @click="leaveRoom()">
+              <span class="fas fa-sign-out-alt"></span>
+              Выйти
+            </button>
+          </div>
         </div>
       </main>
       <footer>
@@ -64,13 +66,73 @@
         </button>
       </header>
       <main>
-        <label>
-          <select class="select btn-bordered btn-transparent">
-            <option>Пока не кончатся слова</option>
-            <option>Заданное число кругов</option>
-          </select>
-        </label>
+        <div class="scrollable">
+          <div class="layer">
+            <h3 class="label w-100">Играть</h3>
+            <label class="field w-300">
+              <select class="select btn-bordered btn-transparent">
+                <option>Пока не кончатся слова</option>
+                <option>Заданное число кругов</option>
+              </select>
+            </label>
+          </div>
+          <div class="layer">
+            <h3 class="label w-100">Слова</h3>
+            <label class="field w-300">
+              <select class="select btn-bordered btn-transparent">
+                <option>Русские, 14141 слово</option>
+                <option>Английские, 1525 слов</option>
+                <option>Простые русские, 4627</option>
+                <option>Средние русские, 4506</option>
+                <option>Сложные русские, 4599</option>
+                <option>Загрузить</option>
+                <option>От каждого игрока</option>
+              </select>
+            </label>
+          </div>
+          <div class="layer">
+            <h3 class="label w-300">Число слов в шляпе</h3>
+            <label class="field w-300">
+              <input class="input">
+            </label>
+          </div>
+          <div class="layer">
+            <h3 class="label w-300">Количество кругов</h3>
+            <label class="field w-300">
+              <input class="input">
+            </label>
+          </div>
+          <div class="layer">
+            <h3 class="label w-300">Формат времени (сек)</h3>
+            <label class="field w-300">
+              <input class="input w-70">
+              <span>+</span>
+              <input class="input w-70">
+              <span>+</span>
+              <input class="input w-70">
+            </label>
+          </div>
+          <div class="layer-detached">
+            <div class="checkbox">
+              <input
+                  type="checkbox"
+                  id="strictModeCheckbox">
+              <label for="strictModeCheckbox">
+                <span class="fas fa-check"></span>
+              </label>
+              <label for="strictModeCheckbox">
+                Строгий режим
+              </label>
+            </div>
+          </div>
+        </div>
       </main>
+      <footer>
+        <button
+            class="btn btn-blue">
+          Сохранить
+        </button>
+      </footer>
     </article>
   </div>
   </body>
