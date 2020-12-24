273a38967b1da552e0cb8e0cd35ab792bbe5f845
Ivan Gaidai-Turlov <gtivansan@gmail.com>
2020-12-24 21:45:02 +0300

    Made correct settings logic

--- src/components/settingsSection.vue
+++ src/components/settingsSection.vue
@@ -26,18 +26,24 @@
           <label class="field w-300">
             <select
                 class="select btn-bordered btn-transparent"
-                v-model="settings.dictionaryId">
-              <option :value="0">Русские, 14141 слово</option>
-              <option :value="1">Английские, 1525 слов</option>
-              <option :value="2">Простые русские, 4627</option>
-              <option :value="3">Средние русские, 4506</option>
-              <option :value="4">Сложные русские, 4599</option>
-              <option :value="5">Загрузить</option>
-              <option :value="6">От каждого игрока</option>
+                v-model="settings.wordsetSource">
+              <option :value="['serverDictionary', 0]">Русские, 14141 слово</option>
+              <option :value="['serverDictionary', 1]">Английские, 1525 слов</option>
+              <option :value="['serverDictionary', 2]">Простые русские, 4627</option>
+              <option :value="['serverDictionary', 3]">Средние русские, 4506</option>
+              <option :value="['serverDictionary', 4]">Сложные русские, 4599</option>
+              <option :value="['hostDictionary']">Загрузить</option>
+              <option :value="['playerWords']">От каждого игрока</option>
             </select>
           </label>
         </div>
-        <div class="layer" v-show="settings.termCondition === 'words'">
+        <div class="layer" v-show="settings.wordsetSource[0] === 'hostDictionary'">
+          <h3 class="label w-250">Загрузить словарь</h3>
+        </div>
+        <div
+            class="layer"
+            v-show="settings.termCondition === 'words' &&
+              settings.wordsetSource[0] !== 'playerWords'">
           <h3 class="label w-250">Число слов в шляпе</h3>
           <label class="field w-300 w-70-mobile">
             <input class="input" v-model.number="settings.wordNumber">
@@ -95,40 +101,101 @@
 import app from "src/app.js";
 import store from "src/store.js";
 
+const room = store.state.room;
+
 export default {
   data: function () {
     return {
-      settings: {
-        ...store.state.room.settings,
-        delayTime: store.state.room.settings.delayTime / 1000,
-        explanationTime: store.state.room.settings.explanationTime / 1000,
-        aftermathTime: store.state.room.settings.aftermathTime / 1000
-      }
+      settings: {}
     };
   },
   computed: {
     serverSettings: function () {
-      return store.state.room.settings;
+      return room.settings;
     }
   },
   methods: {
     applySettings() {
-      app.applySettings({
-        ...this.settings,
-        delayTime: this.settings.delayTime * 1000,
-        explanationTime: this.settings.explanationTime * 1000,
-        aftermathTime: this.settings.aftermathTime * 1000
-      });
+      app.applySettings(this.storeFromLocalSettings());
     },
+
+    // Есть два представления для настроек.
+    // store - то, как настройки хранятмся
+    // в данный момеент на сервере.
+    // local - то, как настройки хранятся
+    // в формах, в которые их ввёл пользователь.
+    // Следующие две функции преобразуют настройки
+    // из одного представления в другое.
+    localFromStoreSettings() {
+      let res = {};
+
+      // На сервере время хранится в миллисекундах
+      // В форме для пользователя его нужно вводить в секундах
+      res.delayTime = room.settings.delayTime / 1000;
+      res.explanationTime = room.settings.explanationTime / 1000;
+      res.aftermathTime = room.settings.aftermathTime / 1000;
+
+      res.strictMode = room.settings.strictMode;
+      res.termCondition = room.settings.termCondition;
+
+      // В данном случае wordsetSource это пара из wordsetType и доп. опций.
+      // Если wordset набирается из словаря с сервера, то необходимо
+      // в качестве этой доп. опции указать id словаря.
+      // Во всех остальных случаях доп. опции отсутсвуют, поэтому
+      // второй элемент пары опускается.
+      // Это используется для возможности в одном поле выбора <select>
+      // указывать как специфичные источники слов, так и конкретные словари
+      // с сервера.
+      res.wordsetSource = [room.settings.wordsetType];
+      if (room.settings.wordsetType === "serverDictionary") {
+        res.wordsetSource.push(room.settings.dictionaryId);
+      }
+      if (room.settings.termCondition === "words" &&
+        room.settings.wordsetType !== "playerWords") {
+        res.wordNumber = room.settings.wordNumber;
+      }
+      if (room.settings.termCondition === "turns") {
+        res.turnsNumber = room.settings.turnsNumber;
+      }
+      if (room.settings.wordsetType === "hostDictionary") {
+        res.dictionaryFileInfo = room.settings.dictionaryFileInfo;
+      }
+      return res;
+    },
+    storeFromLocalSettings() {
+      let res = {};
+
+      // На сервере время хранится в миллисекундах
+      // В форме для пользователя его нужно вводить в секундах
+      res.delayTime = this.settings.delayTime * 1000;
+      res.explanationTime = this.settings.explanationTime * 1000;
+      res.aftermathTime = this.settings.aftermathTime * 1000;
+
+      res.strictMode = this.settings.strictMode;
+      res.termCondition = this.settings.termCondition;
+      res.wordsetType = this.settings.wordsetSource[0];
+      if (res.wordsetType === "serverDictionary") {
+        res.dictionaryId = this.settings.wordsetSource[1];
+      }
+      if (res.termCondition === "words" &&
+        res.wordsetType !== "playerWords") {
+        res.wordNumber = this.settings.wordNumber;
+      }
+      if (res.termCondition === "turns") {
+        res.turnsNumber = this.settings.turnsNumber;
+      }
+      if (res.wordsetType === "hostDictionary") {
+        res.dictionaryFileInfo = this.settings.dictionaryFileInfo;
+      }
+      return res;
+    }
+  },
+  created: function() {
+    this.settings = this.localFromStoreSettings();
   },
   watch: {
     serverSettings: function () {
-      this.settings = {
-        ...store.state.room.settings,
-        delayTime: store.state.room.settings.delayTime / 1000,
-        explanationTime: store.state.room.settings.explanationTime / 1000,
-        aftermathTime: store.state.room.settings.aftermathTime / 1000
-      };
+      this.settings = this.localFromStoreSettings();
     }
   }
 };
