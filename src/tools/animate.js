0f70c24ed6a78bb40d98af4324e14bfff40fccf5
Ivan Gaidai-Turlov <gtivansan@gmail.com>
2021-01-26 14:46:45 +0300

    Fixed error in console
    turnTimer.vue?055e:91 Uncaught TypeError: Cannot read property 'delayTime' of null

--- src/tools/animate.js
+++ src/tools/animate.js
@@ -7,12 +7,12 @@ export function animate({startTime, timing, draw, duration, stopCondition}) {
     return new Promise(function (resolve) {
         let start = startTime;
         requestAnimationFrame(function animate() {
+            if (stopCondition()) return;
             let time = timeSync.getTime();
             let timeFraction = (time - start) / duration;
             if (timeFraction > 1) timeFraction = 1;
             let progress = timing(timeFraction);
             draw(progress);
-            if (stopCondition()) return;
             if (timeFraction < 1) {
                 requestAnimationFrame(animate);
             } else {
