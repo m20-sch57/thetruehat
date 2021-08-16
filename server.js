7a64b9383cc1776be24baac419cff5fe3e70e46d
Gleb Minaev <43728100+lounres@users.noreply.github.com>
2021-08-16 23:29:38 +0300

    Added logging count of online users on connects and disconnects.

--- server.js
+++ server.js
@@ -1937,7 +1937,7 @@ class Callbacks {
 
 io.on("connection", function(socket) {
     if (WRITE_LOGS) {
-        console.log(socket.id, "connection", undefined);
+        console.log(socket.id, "connection", `(${Object.keys(io.sockets.connected).length} online)`);
     }
 
     /**
@@ -2186,7 +2186,7 @@ io.on("connection", function(socket) {
 
     socket.on("disconnect", function() {
         if (WRITE_LOGS) {
-            console.log(socket.id, "disconnect", undefined);
+            console.log(socket.id, "disconnect", `(${Object.keys(io.sockets.connected).length} online)`);
         }
 
         Callbacks.disconnect(socket)
